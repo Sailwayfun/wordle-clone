53bd8d2a98d394211b5877c0efe2896de8088709
Sailwayfun <liaoleon000513@gmail.com>
2023-10-30 19:01:53 +0800

    set up TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  
+
+  return (
+    <>
+    <h1>Wordle</h1>
+    </>
+  )
+}
+
+export default App
--- src/README.md
+++
@@ -1,2 +0,0 @@
-# Front-End-Class-DiveIntoTopic
-Front End Class - Dive Into Topic
