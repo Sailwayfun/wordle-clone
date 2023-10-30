6c36271e92547c2ea81632d753f367306c091dab
Sailwayfun <liaoleon000513@gmail.com>
2023-10-30 22:51:28 +0800

    fix: Wrapper padding to 0.75rem

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -3,7 +3,7 @@ interface WrapperProps {
 }
 const Wrapper = ({children}:WrapperProps) => {
     return (
-        <div className="w-4/5 mx-auto mt-5 bg-[#121213] p-2 text-white xl:max-w-6xl">
+        <div className="w-4/5 mx-auto mt-5 bg-[#121213] p-3 text-white xl:max-w-6xl">
             {children}
         </div>
     )
