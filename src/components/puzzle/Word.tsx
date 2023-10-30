64c1947f47250e7dae2a9fd96502ad36135c9a56
Sailwayfun <liaoleon000513@gmail.com>
2023-10-30 22:49:59 +0800

    fix: x-gaps and y-gaps to 0.75rem

--- src/components/puzzle/Word.tsx
+++ src/components/puzzle/Word.tsx
@@ -5,7 +5,7 @@ interface RowProps {
 }
 const Word = ({colCount, word}: RowProps) => {
     return (
-        <div className="w-full grid grid-cols-5 gap-1">
+        <div className="w-full grid grid-cols-5 gap-3">
             {colCount && word.map((piece:string, index:number) => (<Piece key={index} text={piece}/>))}
         </div>
     )
--- src/components/puzzle/index.tsx
+++ src/components/puzzle/index.tsx
@@ -6,7 +6,7 @@ interface PuzzleProps {
 const Puzzle = ({words}:PuzzleProps) => {
     const colCount:number = 5;
     return (
-        <div className="w-full space-y-1">
+        <div className="w-full space-y-3">
             {words.map((word:string[], index:number) => {
                 return <Word key={index} word={word} colCount={colCount}/>
             })}
