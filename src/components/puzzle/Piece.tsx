662c2b0ea30a04e01058bc1ca17ab465dc4b9d53
Sailwayfun <liaoleon000513@gmail.com>
2023-10-30 22:19:16 +0800

    fix: group Word into a component

--- src/components/puzzle/Piece.tsx
+++ src/components/puzzle/Piece.tsx
@@ -1,6 +1,6 @@
 const Piece = () => {
     return (
-        <div className=" bg-transparent border border-gray-300 aspect-square shadow rounded-md"></div>
+        <div className=" bg-transparent border border-gray-300 aspect-square shadow rounded-sm"></div>
     )
 }
 
--- src/components/puzzle/index.tsx
+++ src/components/puzzle/index.tsx
@@ -1,10 +1,24 @@
 import Piece from "./Piece";
-const Puzzle = () => {
+
+interface RowProps {
+    colCount: number;
+}
+const Word = ({colCount}: RowProps) => {
     return (
-        <div className="w-full grid grid-row-6 grid-cols-5 gap-3">
-            {Array.from(Array(30).keys()).map((i) => (<Piece key={i}/>))}
+        <div className="w-full grid grid-cols-5 gap-1">
+            {colCount && Array.from(Array(colCount).keys()).map((i) => (<Piece key={i}/>))}
         </div>
     )
-}
+};
+
+const Puzzle = () => {
+    const rowCount:number = 6;
+    const colCount:number = 5;
+    return (
+        <div className="w-full space-y-1">
+            {Array.from(Array(rowCount).keys()).map((i) => (<Word key={i} colCount={colCount}/>))}
+        </div>
+    )
+};
 
 export default Puzzle;
