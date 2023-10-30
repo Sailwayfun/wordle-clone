eac312250358531c33a63d3669ea9c6f8873ee21
Sailwayfun <liaoleon000513@gmail.com>
2023-10-30 22:25:35 +0800

    make Word component independent

--- src/components/puzzle/index.tsx
+++ src/components/puzzle/index.tsx
@@ -1,15 +1,4 @@
-import Piece from "./Piece";
-
-interface RowProps {
-    colCount: number;
-}
-const Word = ({colCount}: RowProps) => {
-    return (
-        <div className="w-full grid grid-cols-5 gap-1">
-            {colCount && Array.from(Array(colCount).keys()).map((i) => (<Piece key={i}/>))}
-        </div>
-    )
-};
+import Word from "./Word";
 
 const Puzzle = () => {
     const rowCount:number = 6;
