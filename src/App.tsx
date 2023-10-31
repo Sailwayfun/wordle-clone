6156611f33576f4a9b1a84612460dd558b1da435
Sailwayfun <liaoleon000513@gmail.com>
2023-10-31 16:49:19 +0800

    deploy phase-2

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 import { useState } from "react";
 function App() {
   const answer:string[]=["G", "R", "A", "I", "L"];
-  const [attempts, setAttempts] = useState<string[][]>([
+  const [attempts] = useState<string[][]>([
     ["W", "A", "T", "E", "R"],
     ["A", "F", "T", "E", "R"],
     ["B", "R", "A", "I", "L"],
