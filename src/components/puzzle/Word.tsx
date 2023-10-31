28d7f64331eaf6b4a98d58862722149c02d3676d
Sailwayfun <liaoleon000513@gmail.com>
2023-10-31 18:51:58 +0800

    extract status logic

--- src/components/puzzle/Word.tsx
+++ src/components/puzzle/Word.tsx
@@ -7,13 +7,12 @@ const Word = ({word, answer}: RowProps) => {
     return (
         <div className="w-full grid grid-cols-5 gap-[10px] sm:gap-3">
             {word.map((letter:string, index:number) => {
-                return letter === "" ? (
-                    <Piece key={index} text="" status="empty" />
-                ) : answer[index] === letter ? 
-                (<Piece key={index} text={letter} status="correct" />) :
-                answer.includes(letter) ? (<Piece key={index} text={letter} status="partial" />) :
-                (<Piece key={index} text={letter} status="absent" />)
-                })}
+                const status = letter === "" ? "empty" : 
+                answer[index] === letter ? "correct" : 
+                answer.includes(letter) ? "partial" : "absent";
+                return (<Piece key={index} text={letter} status={status}/>)
+                })
+            }
         </div>
     );
 };
