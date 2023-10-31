77bc2a841b96e0cc5abad38d89532d41de2957ce
Sailwayfun <liaoleon000513@gmail.com>
2023-11-01 07:32:17 +0800

    add keyup listener in useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import Wrapper from "./components/UI/Wrapper";
 import Puzzle from "./components/Puzzle";
 import Header from "./components/Header";
-import { useReducer, Reducer } from "react";
+import { useReducer, Reducer, useEffect } from "react";
 import puzzleReducer, { initialState as initialAttempts, State, Action, colCount, rowCount } from "./store/puzzleReducer";
 function App() {
   const answer:string[]=["G", "R", "A", "I", "L"];
   const [state, dispatch] = useReducer<Reducer<State, Action>>(puzzleReducer, initialAttempts);
   const {attempts, currentRow, currentGuess} = state;
+  useEffect(() =>{
+    function handleKeyUp(e:KeyboardEvent) {
+
+    }
+    window.addEventListener("keyup", handleKeyUp);
+    return () => {
+      window.removeEventListener("keyup", handleKeyUp);
+    }
+  }, []);
   return (
     <>
       <Header />
