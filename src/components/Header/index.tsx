ea8b9698f4d00a96abb9cd7c7126aff0f742ed03
Sailwayfun <liaoleon000513@gmail.com>
2025-04-27 02:53:12 +0800

    fix: main heading aligned a bit too left

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,14 +6,16 @@ import ThemeToggle from "../UI/ThemeToggle";
 
 const Header = () => {
     return(
-        <header className="w-full py-4 border-b-2 dark:border-dark-gray border-gray-300 items-center uppercase tracking-widest flex gap-5 pr-10">
+        <header className="w-full py-4 border-b-2 dark:border-dark-gray border-gray-300 items-center uppercase tracking-widest flex gap-5 pr-10 relative">
             <Heading text="Wordle 2"/>
-            <ThemeToggle />
-            <Link to="/" viewTransition>
-                <span className="dark:text-white text-xl hover:text-bronze transition-colors">
+            <div className="absolute md:right-4 right-0 flex items-center gap-2">
+                <ThemeToggle />
+                <Link to="/" viewTransition>
+                    <span className="dark:text-white text-xl hover:text-bronze transition-colors">
                     <InfoIcon />
                 </span>
             </Link>
+            </div>
         </header>
     );
 };
